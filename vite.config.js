0d190605bd5b65b8425cf2d67205da2f91908720
Murtaza Mazhar <murtazamazhar7798@gmail.com>
2025-07-09 13:31:55 +0500

    update

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
+export default defineConfig({
+  base: '/quote-generator/',
   server: {
     proxy: {
       '/api': {
@@ -12,4 +13,4 @@ export default {
       }
     }
   }
-};
+});
